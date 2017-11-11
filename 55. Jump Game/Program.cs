58c332c2b50238f58745a145fb72581dbce2d5cd
Jason Wu <jsw7524@gmail.com>
2017-11-11 11:52:30 +0800

    ad-hoc

--- 55. Jump Game/Program.cs
+++ 55. Jump Game/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace _55.Jump_Game
 {
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (null == nums)
+            {
+                return false;
+            }
+            int maxDistance= 0;
+            for (int i=0;i< nums.Length-1 && maxDistance < nums.Length&& i<=maxDistance; i++)
+            {
+                if (maxDistance < i + nums[i])
+                {
+                    maxDistance = i + nums[i];
+                }
+            }
+            if (maxDistance+1 >= nums.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            (new Solution()).CanJump(new int[] {0, 2, 3});
         }
     }
 }
